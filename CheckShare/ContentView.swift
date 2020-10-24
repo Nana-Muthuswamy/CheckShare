3c6fb453b0010f03a2a8b501711913c10819fe3a
Narayanan Muthuswamy <narayanan_vm@apple.com>
2020-10-24 18:13:35 -0500

    CheckShare Basic Version

--- CheckShare/ContentView.swift
+++ CheckShare/ContentView.swift
@@ -8,9 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmountDisplay = ""
+    @State private var totalPersons = 0 {
+        didSet {
+            print("Total persons updated")
+        }
+    }
+    @State private var selectedTipPercentage = 2
+
+    let tipPercentages = [0, 10, 15, 20, 25]
+
+    var billAmount: Double {
+        return Double(billAmountDisplay) ?? 0
+    }
+
+    var tipAmount: Double {
+        guard selectedTipPercentage < tipPercentages.count else { return 0 }
+        return (billAmount * Double(tipPercentages[selectedTipPercentage])) / 100
+    }
+
+    var totalBillAmount: Double {
+        return billAmount + tipAmount
+    }
+
+    var totalPerPerson: Double {
+        guard (totalPersons + 2) > 0 else { return totalBillAmount }
+        return totalBillAmount / Double(totalPersons + 2)
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter Check Amount", text: $billAmountDisplay)
+                        .keyboardType(.decimalPad)
+
+                    Picker("Number of people", selection: $totalPersons) {
+                        ForEach(2 ..< 21) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+
+                Section(header: Text("Tip Percentage")) {
+                    Picker("Tip Percentage", selection: $selectedTipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+
+                Section(header: Text("Totals")) {
+                    HStack{
+                        Text("Amount")
+                        Spacer()
+                        Text("$\(totalBillAmount, specifier: "%.2f")")
+                    }
+
+                    HStack{
+                        Text("Share")
+                        Spacer()
+                        Text("$\(totalPerPerson, specifier: "%.2f")")
+                    }
+                }
+            }
+            .navigationBarTitle("Check Share")
+        }
     }
 }
 
