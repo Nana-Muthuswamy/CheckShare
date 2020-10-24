437287469112163c18607bdcdcaf89e402ad0541
Narayanan Muthuswamy <narayanan_vm@apple.com>
2020-10-24 16:42:37 -0500

    Initial Commit

---
+++ CheckShare/CheckShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckShareApp.swift
+//  CheckShare
+//
+//  Created by Narayanan V Muthuswamy on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct CheckShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
